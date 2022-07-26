99a223e8836d84b2a714dc7d84275f20c04623d1
Matthias K. F. Godinho <post@mkfg.de>
2022-07-26 19:14:37 +0200

    use correct icons in comment

--- keyboards/planck/keymaps/ladz/keymap.c
+++ keyboards/planck/keymaps/ladz/keymap.c
@@ -56,20 +56,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |  ⎋  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  ⌫  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |^/tab |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | ^/⇥ |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RS/En |
+ * |   ⇧  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | ⇧/↵  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite|   ^  | Alt  | GUI  |Lower |TD:Space/Dot |Raise | Left | Down |  Up  |Right |
+ * | Brite|   ^  |   ⌥  |   ⌘  |   ▼  |     ␣/.     |   ▲  |   ←  |   ↓  |   ↑  |   →  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    MT(MOD_LCTL, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT  ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   TD(TD_SPACE_DOT),  TD(TD_SPACE_DOT),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT  ,
+    BACKLIT,              KC_LCTL, KC_LALT, KC_LGUI, LOWER,   TD(TD_SPACE_DOT),  TD(TD_SPACE_DOT),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
